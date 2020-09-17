3ecb5e55261cce487e8211d3c3bea5a35e24486a
FKYnJYQ <loveress01@gmail.com>
2020-09-18 01:16:21 +0800

    refactor(homepage): 修改 default 设定
    Signed-off-by: FKYnJYQ <loveress01@gmail.com>

---
+++ docs/other-contributors.md
@@ -0,0 +1,4 @@
+---
+id: contributors
+title: 贡献者 && 贡献者指南
+---
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,101 +1,89 @@
 module.exports = {
-  title: '龙芯杯指北',
-  tagline: 'What I cannot create, I do not understand.',
-  url: 'https://nscscc.fkynjyq.com',
-  baseUrl: '/',
-  favicon: 'img/favicon.ico',
-  organizationName: 'fky2015', // Usually your GitHub org/user name.
-  projectName: 'roadmap-of-nscscc', // Usually your repo name.
+  title: "龙芯杯指北",
+  tagline: "What I cannot create, I do not understand.",
+  url: "https://nscscc.fkynjyq.com",
+  baseUrl: "/",
+  favicon: "img/favicon.ico",
+  organizationName: "fky2015", // Usually your GitHub org/user name.
+  projectName: "roadmap-of-nscscc", // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: '龙芯杯指北',
+      title: "龙芯杯指北",
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: "My Site Logo",
+        src: "img/logo.svg",
       },
       links: [
         {
-          to: 'docs/doc1',
-          activeBasePath: 'docs',
-          label: '文档',
-          position: 'left',
+          to: "install-vivado",
+          activeBasePath: "docs",
+          label: "文档",
+          position: "left",
         },
-        // {to: 'blog', label: 'Blog', position: 'left'},
+         {to: 'contributors', label: '贡献者指南', position: 'right'},
         {
-          href: 'https://github.com/fky2015/roadmap-of-nscscc',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/fky2015/roadmap-of-nscscc",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Docs',
+          title: "官方网站",
           items: [
             {
-              label: 'Style Guide',
-              to: 'docs/doc1',
-            },
-            {
-              label: 'Second Doc',
-              to: 'docs/doc2',
+              label: "龙芯杯官方网站",
+              href: "http://www.nscscc.org/",
             },
           ],
         },
         {
-          title: 'Community',
+          title: "参考文档",
           items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
           ],
         },
         {
-          title: 'More',
+          title: "更多",
           items: [
             {
-              label: 'Blog',
-              to: 'blog',
+              label: "北理工 MIPS 相关系列实验（小学期）",
+              href: "https://github.com/bit-mips/bitmips_experiments_doc",
             },
             {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              label: "针对参加龙芯杯的若干建议",
+              href: "https://github.com/Silverster98/bit_nscscc_suggestion",
+            },
+            {
+              label: "GitHub",
+              href: "https://github.com/fky2015/roadmap-of-nscscc",
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Beijing Institute of Technology. Built with Docusaurus.`,
     },
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            "https://github.com/fky2015/roadmap-of-nscscc/edit/master/website/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            "https://github.com/fky2015/roadmap-of-nscscc/edit/master/website/blog/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
